9d39c78bcf0d00c294b3ea2f94edc5153bb531c3
Eric Hartman <imerichartman@gmail.com>
2017-01-04 15:36:33 -0500

    Remove secure pw and first attempt at location rating

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,9 @@ class Location < ApplicationRecord
   belongs_to :user
   belongs_to :company
   has_many :ratings
+
+  def location_rating
+    Rating.where(location_id: self.id).average("rating")
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
   has_many :locations
   has_many :ratings
-  has_secure_password
 end
