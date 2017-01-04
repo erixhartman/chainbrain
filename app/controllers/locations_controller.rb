0fcf7977a1547ab36e2cc5b48de9bc8126320200
Eric Hartman <imerichartman@gmail.com>
2017-01-04 17:13:57 -0500

    Add @location to location controller :

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,53 @@
 class LocationsController < ApplicationController
+  def index
+    @locations = Location.all
+  end
+
+  def show
+    @location  = Location.find(params[:id])
+    if current_user
+      @rating = @location.reviews.build
+    end
+  end
+
+  def create
+    @location = Location.new(location_params)
+
+    if @location.save
+      redirect_to locations_url
+    else
+      render :new
+    end
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+
+    if @location.update_attributes(location_params)
+      redirect_to location_url(@location)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @location  = Location.find(params[:id])
+    @location.destroy
+    redirect_to locations_url
+  end
+
+
+  private
+  def location_params
+    params.require(:location).permit(:address, :rating)
+  end
+
 end
