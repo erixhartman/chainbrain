ab009a47a4fdc830641d8c2b284f1bc80de3c6a7
Eric Hartman <imerichartman@gmail.com>
2017-01-16 14:29:02 -0500

    Rename ratings controller to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103194227) do
+ActiveRecord::Schema.define(version: 20170116192701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20170103194227) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "ratings", force: :cascade do |t|
+  create_table "reviews", force: :cascade do |t|
     t.integer  "rating"
     t.integer  "user_id"
     t.integer  "location_id"
