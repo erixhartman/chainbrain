73a23ad56f01820c0d75eb670ab6459e10c5ac7b
Eric Hartman <imerichartman@gmail.com>
2017-01-23 18:44:50 -0500

    Add new, create, params to company controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,21 @@
 class CompaniesController < ApplicationController
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.build(company_params)
+
+    if @company.save
+      redirect_to locations_path, notice: "Company created"
+    else
+      redirect_to root_path
+    end
+  end
+
+private
+ def picture_params
+   params.require(:company).permit(:name)
+ end
+
 end
