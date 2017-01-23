af7d60000b0b88892f392c27f5e02c5f253d0199
Eric Hartman <imerichartman@gmail.com>
2017-01-23 18:21:06 -0500

    Log in/out process created and works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,18 +6,16 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      if user.teacher || user.student
-        redirect_to user_path(current_user)
-      else
-        redirect_to root_path
-      end
+      redirect_to locations_path, notice: "Logged In!"
     else
+      flash.now[:alert] = "Invalid User Name or Password"
       render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url
+    #redirect_to root_url
+    redirect_to locations_path
   end
 end
