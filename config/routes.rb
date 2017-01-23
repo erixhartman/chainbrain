c633eafc202c6d38cd13460b1d21e5a37dac8b3d
Eric Hartman <imerichartman@gmail.com>
2017-01-23 12:32:13 -0500

    Update routes to only ones needed for each model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :locations
-  resources :reviews
-  resources :companies
-  resources :users
+  resources :locations, only: [:index, :show, :new, :create, :edit, :update]
+  resources :reviews, only: [:show, :new, :create]
+  resources :companies, only: [:index, :show, :new, :create]
+  resources :users, only: [:new, :create, :show]
 end
