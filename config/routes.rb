5c1095782a9526b5fd730e871a79bb0026227381
Eric Hartman <imerichartman@gmail.com>
2017-01-25 16:17:44 -0500

    reroute reviews through locations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :locations, only: [:index, :show, :new, :create, :edit, :update]
-  resources :reviews, only: [:show, :new, :create]
+  resources :locations, only: [:index, :show, :new, :create, :edit, :update] do
+    resources :reviews, only: [:show, :new, :create]
+  end
   resources :companies, only: [:index, :show, :new, :create]
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
