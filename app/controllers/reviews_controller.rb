39e4ad94458fbe587a14bd3d8eb5e6c1297379df
Eric Hartman <imerichartman@gmail.com>
2017-01-25 16:18:19 -0500

    Fill in review controller and fix link on location show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @location = Location.find(params[:location_id])
+  end
+
+  def create
+    @review = @location.reviews.build(review_params)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to location_path(@location.id)
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating)
+  end
+
+  def load_location
+    @location = Location.find(params[:location_id])
+  end
 end
